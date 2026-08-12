5f936dfe533e04a30fb65554125b20afa498af50
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2026-08-12 14:10:28 -0300

    feat: initialize Vue router with home and products routes

--- curavida-web/src/router/index.js
+++ curavida-web/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
 import ProductsView from '../views/ProductsView.vue'
 
 const router = createRouter({
