29dc8713be8c51ea041bdebc51765b7125d99fdd
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2026-08-18 13:43:34 -0300

    feat: add getCategories service to fetch category data from API

--- curavida-web/src/services/category.service.js
+++ curavida-web/src/services/category.service.js
@@ -1,13 +1,7 @@
-import API_URL from './api'
+import api from './api'
 
 export const getCategories = async () => {
-  const response = await fetch(`${API_URL}/categories`)
+  const response = await api.get('/categories')
 
-  if (!response.ok) {
-    throw new Error('Erro ao buscar categorias.')
-  }
-
-  const result = await response.json()
-
-  return result.data
+  return response.data.data
 }
