7f5606748cffd5ff648088baffa8cda142857229
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2026-08-19 12:59:19 -0300

    feat: initialize project with Vite, Vue, PWA support, and essential dependencies

--- curavida-web/vite.config.js
+++ curavida-web/vite.config.js
@@ -3,12 +3,45 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'CuraVida',
+        short_name: 'CuraVida',
+        description: 'CuraVida - Produtos e cuidados para sua saúde',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
